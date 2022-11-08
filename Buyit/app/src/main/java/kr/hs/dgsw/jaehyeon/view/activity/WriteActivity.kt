3c97c28f69cce8f7fb796f38386eeacc230e6a3f
임재현 <ljh040513@naver.com>
2022-11-08 10:56:44 +0900

    [ADD] 분석, 등록 페이지 xml add

---
+++ Buyit/app/src/main/java/kr/hs/dgsw/jaehyeon/view/activity/WriteActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.jaehyeon.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.jaehyeon.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
